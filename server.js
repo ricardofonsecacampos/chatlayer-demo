6a4df346635f254f06335da89ada257d8e1fe7be
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2021-03-27 17:53:54 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,40 @@
+// HTTP server to link the application HTML page to services in NodeJS functions.
+
+// Http server module.
+const http = require('http')
+// Serve static files.
+const fs = require('fs')
+// Parse request parameters.
+const {parse} = require('querystring');
+
+// use the port Heroku indicates
+const PORT = process.env.PORT || 3000
+
+
+function getPostJsonParams(request, callback) {
+	if (request.method == 'POST') {
+		let body = ''
+		request.on('data', chunk => {body += chunk.toString()})
+		request.on('end', () => callback(JSON.parse(body)))
+	}
+}
+
+const server = http.createServer((req, res) => {
+	let location = 'frontend'
+	let serveFile = true
+	let contentType = 'application/json'
+  
+			contentType = 'text/html'
+			location += '/chatlayer-demo.html'
+			break;
+  
+	// starts writing the response.
+	res.writeHead(200, { 'content-type': contentType })
+	
+	// serve the requested file.
+	if (serveFile) {
+		fs.createReadStream(location).pipe(res)
+	}
+}).listen(PORT)
+
+console.log('Node server running on port ' + PORT)
