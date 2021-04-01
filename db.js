dabadbdf2a1ac41cb24a03cd0a8824acbce3ef14
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2021-04-01 04:12:35 -0300

    Update db.js

--- db.js
+++ db.js
@@ -14,19 +14,17 @@ pool.on('connect', () => console.log('db operation'));
 pool.query("create table if not exists roof_order (product varchar(40) not null, quantity smallint not null, order_date date not null)",
            (err, res) => {
   if (err) console.log(err)
-  pool.end()
 });
 
 //pool.query("select * from pg_catalog.pg_database",
-pool.query("insert into roof_order (product, quantity, order_date) values ('tiles', 25, current_date)",
-           (err, res) => {
-  if (err) console.log(err)
-  pool.end()
-});
+//pool.query("insert into roof_order (product, quantity, order_date) values ('tiles', 25, current_date)",
+//           (err, res) => {
+//  if (err) console.log(err)
+//});
 
 pool.query("select * from roof_order",
            (err, res) => {
   if (err) console.log(err)
-  console.log(res)
-  pool.end()
+  else console.log(res.rows)
+  //pool.end()
 });
