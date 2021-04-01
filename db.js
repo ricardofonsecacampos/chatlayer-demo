1aa48d2e9c08bbb062637c1f5f60d944b47c5a9b
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2021-04-01 03:47:31 -0300

    Update db.js

--- db.js
+++ db.js
@@ -13,8 +13,13 @@ const pool = new Pool(connectionString);
 pool.on('connect', () => console.log('connected to db'));
 
 pool.query("create table if not exists roof_order (product varchar(40) not null, quantity smallint not null, order_date date not null)",
+           (err, res) => {
+  console.log(err)
+});
+
 //pool.query("select * from pg_catalog.pg_database",
+pool.query("select * from roof_order",
            (err, res) => {
   console.log(err, res)
-  pool.end()
+  //pool.end()
 });
