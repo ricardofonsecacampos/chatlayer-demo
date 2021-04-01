afe1962dbea6b1723760e10baec04d3e3d8c606a
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2021-04-01 04:55:42 -0300

    Update db.js

--- db.js
+++ db.js
@@ -36,16 +36,18 @@ pool.on('connect', () => console.log('db operation'));
 
 // assures the order table is OK.
 //pool.query("drop table roof_order",
-pool.query("create table if not exists roof_order (product varchar(40) not null, quantity smallint not null, order_date datetime not null)",
+pool.query("create table if not exists roof_order (product varchar(40) not null, quantity smallint not null, order_date timestamp not null)",
            (err, res) => {
   if (err) console.log(err)
   
-  pool.query("insert into roof_order (product, quantity, order_date) values ('tiles', 25, current_date)",
-             (err1, res1) => {
-    if (err1) console.log(err1)
-  });
+  pool.query("commit", (err1, res1) => { });
   
-  pool.query("commit", (err2, res2) => { });
+  //pool.query("insert into roof_order (product, quantity, order_date) values ('tiles', 25, current_date)",
+  //           (err2, res2) => {
+  //  if (err2) console.log(err2)
+  //});
+  
+  pool.query("commit", (err3, res3) => { });
 });
 
 listOrder((rows) => console.log(rows))
