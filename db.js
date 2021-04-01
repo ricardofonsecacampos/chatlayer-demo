1f447fc4cee5c522e3d0592714368f5375c505e9
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2021-04-01 03:00:42 -0300

    Update db.js

--- db.js
+++ db.js
@@ -7,3 +7,9 @@ connectionString = {
 
 const pool = new Pool(connectionString);
 pool.on('connect', () => console.log('connected to db'));
+
+pool.query("create table if not exists order (product varchar(40) not null, quantity smallint not null, order_date date not null)",
+           (err, res) => {
+  console.log(err, res)
+  pool.end()
+});
