30ee4c224078317a81db443147edd33eee7d3c1e
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2021-04-01 04:26:56 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,20 +1,37 @@
+// import Postgre module.
 const { Pool } = require('pg');
 
+// retrieves the connection data from environment variable.
 connectionString = {
   connectionString: process.env.DATABASE_URL,
   ssl: true
 };
 
+// list all submited orders.
+function listOrder(callback) {
+  pool.query("select * from roof_order", (err, res) => {
+    if (err) console.log(err)
+    else callback(res.rows)
+    //pool.end()
+  });
+}
+
 // avoid self signed certificate problem.
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
 const pool = new Pool(connectionString);
+
+// done before any database operation.
 pool.on('connect', () => console.log('db operation'));
 
-pool.query("create table if not exists roof_order (product varchar(40) not null, quantity smallint not null, order_date date not null)",
+// assures the order table is OK.
+pool.query("drop table roof_order",
+//pool.query("create table if not exists roof_order (product varchar(40) not null, quantity smallint not null, order_date date not null)",
            (err, res) => {
   if (err) console.log(err)
 });
+
+//listOrder((rows) => console.log(rows))
 
 //pool.query("select * from pg_catalog.pg_database",
 //pool.query("insert into roof_order (product, quantity, order_date) values ('tiles', 25, current_date)",
@@ -22,9 +39,4 @@ pool.query("create table if not exists roof_order (product varchar(40) not null,
 //  if (err) console.log(err)
 //});
 
-pool.query("select * from roof_order",
-           (err, res) => {
-  if (err) console.log(err)
-  else console.log(res.rows)
-  //pool.end()
-});
+module.exports = { listOrder }
