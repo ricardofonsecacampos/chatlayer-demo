1cf9b3eed2586af7a2beefd7f6606de336e23e2e
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2021-04-01 03:44:30 -0300

    Update db.js

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 const pool = new Pool(connectionString);
 pool.on('connect', () => console.log('connected to db'));
 
-pool.query("create table roof_order (product varchar(40) not null, quantity smallint not null, order_date date not null)",
+pool.query("create table if not exists roof_order (product varchar(40) not null, quantity smallint not null, order_date date not null)",
 //pool.query("select * from pg_catalog.pg_database",
            (err, res) => {
   console.log(err, res)
