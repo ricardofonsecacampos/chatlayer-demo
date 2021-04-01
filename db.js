0646348f6cbc6c118b8f725b4aeaa2d64f18d5f6
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2021-04-01 05:32:32 -0300

    Update db.js

--- db.js
+++ db.js
@@ -7,8 +7,13 @@ connectionString = {
   ssl: true
 };
 
+let pool = null
+
 // list all submited orders.
 function listOrder(callback) {
+  // it's not right to create the pool always.
+  connect();
+  
   //pool.query("select * from pg_catalog.pg_database",
   pool.query("select * from roof_order", (err, res) => {
     if (err) console.log(err)
@@ -19,6 +24,9 @@ function listOrder(callback) {
 
 // list all submited orders.
 function saveOrder(order) {
+  // it's not right to create the pool always.
+  connect();
+  
   pool.query("insert into roof_order (product, quantity, value, order_date) values ('" + order.product + "', " + order.quantity + ", " + order.value + ", current_date)",
              (err, res) => {
     if (err) console.log(err)
@@ -26,26 +34,31 @@ function saveOrder(order) {
   });
 }
 
+// creates the connection to the database.
+function connect() {
+  pool = new Pool(connectionString);
+  // done before any database operation.
+  pool.on('connect', () => console.log('db operation'));  
+}
+
 // avoid self signed certificate problem.
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
-const pool = new Pool(connectionString);
-
-// done before any database operation.
-pool.on('connect', () => console.log('db operation'));
-
+connect();
 // assures the order table is OK.
 //pool.query("drop table roof_order",
 pool.query("create table if not exists roof_order (product varchar(40) not null, quantity smallint not null, value float not null, order_date timestamp not null)",
            (err, res) => {
   if (err) console.log(err)
-  pool.query("commit", (err1, res1) => { });
-  
-  //pool.query("insert into roof_order (product, quantity, value, order_date) values ('tiles', 25, 180.55, current_date)",
-  //           (err2, res2) => {
-  //  if (err2) console.log(err2)
-  //});
-  //pool.query("commit", (err3, res3) => { });
+  pool.end()
+});
+
+connect();
+// sample data.
+pool.query("insert into roof_order (product, quantity, value, order_date) values ('tiles', 25, 180.55, current_date)",
+           (err, res) => {
+  if (err) console.log(err)
+  pool.end()
 });
 
 listOrder((rows) => console.log(rows))
