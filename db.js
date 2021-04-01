651f97eb4dafee3b605eecf6ec7dc6b4304943a1
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2021-04-01 03:35:40 -0300

    Update db.js

--- db.js
+++ db.js
@@ -12,7 +12,8 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 const pool = new Pool(connectionString);
 pool.on('connect', () => console.log('connected to db'));
 
-pool.query("create table order (product varchar(40) not null, quantity smallint not null, order_date date not null)",
+//pool.query("create table order (product varchar(40) not null, quantity smallint not null, order_date date not null)",
+pool.query("select * from pg_catalog.pg_database",
            (err, res) => {
   console.log(err, res)
   pool.end()
