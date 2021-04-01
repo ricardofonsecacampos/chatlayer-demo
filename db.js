a3f4c015e8536aaee69e0645d38be7da27277922
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2021-04-01 04:28:55 -0300

    Update db.js

--- db.js
+++ db.js
@@ -25,18 +25,18 @@ const pool = new Pool(connectionString);
 pool.on('connect', () => console.log('db operation'));
 
 // assures the order table is OK.
-pool.query("drop table roof_order",
-//pool.query("create table if not exists roof_order (product varchar(40) not null, quantity smallint not null, order_date date not null)",
+//pool.query("drop table roof_order",
+pool.query("create table if not exists roof_order (product varchar(40) not null, quantity smallint not null, order_date date not null)",
            (err, res) => {
   if (err) console.log(err)
 });
 
-//listOrder((rows) => console.log(rows))
-
 //pool.query("select * from pg_catalog.pg_database",
-//pool.query("insert into roof_order (product, quantity, order_date) values ('tiles', 25, current_date)",
-//           (err, res) => {
-//  if (err) console.log(err)
-//});
+pool.query("insert into roof_order (product, quantity, order_date) values ('tiles', 25, current_date)",
+           (err, res) => {
+  if (err) console.log(err)
+});
+
+listOrder((rows) => console.log(rows))
 
 module.exports = { listOrder }
