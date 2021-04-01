44b386799d5dbf00c0d491c34752ae153dc9e957
ricardofonsecacampos <34239257+ricardofonsecacampos@users.noreply.github.com>
2021-04-01 04:02:24 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
 const { Pool } = require('pg');
-console.log('importou pg')
 
 connectionString = {
   connectionString: process.env.DATABASE_URL,
@@ -10,22 +9,23 @@ connectionString = {
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
 const pool = new Pool(connectionString);
-pool.on('connect', () => console.log('connected to db'));
+pool.on('connect', () => console.log('db operation'));
 
 pool.query("create table if not exists roof_order (product varchar(40) not null, quantity smallint not null, order_date date not null)",
            (err, res) => {
   if (err) console.log(err)
+  pool.end()
 });
 
 //pool.query("select * from pg_catalog.pg_database",
 pool.query("insert into roof_order (product, quantity, order_date) values ('tiles', 25, current_date)",
            (err, res) => {
-  console.log(err, res)
-  //pool.end()
+  if (err) console.log(err) else console.log(res)
+  pool.end()
 });
 
 pool.query("select * from roof_order",
            (err, res) => {
-  console.log(err, res)
-  //pool.end()
+  if (err) console.log(err) else console.log(res)
+  pool.end()
 });
